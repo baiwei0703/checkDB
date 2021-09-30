3870fd902d12872e13535da3fee5e8ee43e50aed
baiwei@bailitop.com <baiwei@bailitop.com>
2021-09-30 11:34:09 +0800

    测试，屏蔽记录入库2

--- task_check_db.py
+++ task_check_db.py
@@ -5,7 +5,7 @@ from toolkits.fileTools import GetFileInfo as gf
 
 
 if __name__ == '__main__':
-    db = MyPymysqlPool('db_168_test')
+    db = MyPymysqlPool('db_168')
     check_sql = f"select id,filePath,copyPeriod,remarks from dbBack_info where isEnabled = 1"
     task_list = db.get_all(sql=check_sql)
     now_time = dc.get_now()
@@ -16,27 +16,27 @@ if __name__ == '__main__':
         folderSize = gf.get_dir_size(task['filePath'])
         fileInfo = gf.get_dir_file_number(task['filePath'])
         isNormal = 1
-        errorResson = ''
+        errorReason = ''
         # 如果有上次记录，则进行对比。没有则直接记录
         if last_info:
             if gf.check_path_exist(task['filePath']):
                 if abs(folderSize - task['folderSize']) / task['folderSize'] > 0.05:
-                    errorResson += '备份文件目录大小异常'
+                    errorReason += '备份文件目录大小异常'
                 if len(fileInfo['file_num']) != task['fileNumber']:
-                    errorResson += '备份文件目录数量异常'
+                    errorReason += '备份文件目录数量异常'
                 mtime_check = []
                 for fileName in fileInfo['file_list']:
                     fileMtime = gf.get_file_mtime(task['filePath'], fileName)
                     if dc.date_calc(fileMtime, task['checkDatetime'], task['copyPeriod']):
                         mtime_check.append(1)
                 if not mtime_check:
-                    errorResson += '没有生成任何备份文件'
+                    errorReason += '没有生成任何备份文件'
             else:
-                errorResson = '文件夹不存在'
-            if errorResson:
+                errorReason = '文件夹不存在'
+            if errorReason:
                 isNormal = 0
-        ins_check_ret_sql = f"insert into dbBack_history(taskId, checkDatetime, folderSize, fileNumber, isNormal, errorResson) value (%s,%s,%s,%s,%s,%s)"
-        data = [task['id'], now_time, folderSize, fileInfo['file_num'], isNormal, errorResson]
+        ins_check_ret_sql = f"insert into dbBack_history(taskId, checkDatetime, folderSize, fileNumber, isNormal, errorReason) value (%s,%s,%s,%s,%s,%s)"
+        data = [task['id'], now_time, folderSize, fileInfo['file_num'], isNormal, errorReason]
         print(data)
         db.insert(ins_check_ret_sql, data)
         db.end()
